89163cf543dfccf843e7e0929cc49d2d98050e71
Javier Tomas Zon <jtomaszon@gmail.com>
2014-10-30 14:43:35 -0400

    Fixing dependencies for puppet

--- puppet.pp
+++ puppet.pp
@@ -1,5 +1,21 @@
+$url = "http://apt.puppetlabs.com/puppetlabs-release-trusty.deb"
+$tmpFile = "/tmp/puppetlabs-release-trusty.deb"
+
+exec { 'puppet-release': 
+  command => "/usr/bin/wget $url -qO $tmpFile && /usr/bin/dpkg -i $tmpFile",
+  unless => '/bin/ls /etc/apt/sources.list.d/puppetlabs.list',
+}	
+
+exec { "aptitude update":
+  alias	=> 'apt-udp',
+  command => "/usr/bin/aptitude update",
+  require => Exec['puppet-release'],
+  onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
+}
+
 package { 'puppet':
-  ensure		=> latest,
+  ensure => latest,
+  require => Exec['apt-udp'],
 }
 
 file { 'puppet.conf':
