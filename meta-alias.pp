38256ed06a3b8c268c7ccb5536403535494dd343
Javier Tomas Zon <jtomaszon@gmail.com>
2014-10-30 00:16:27 +0000

    Using subscribe

--- meta-alias.pp
+++ meta-alias.pp
@@ -1,11 +1,17 @@
 file { 'main.cf':
   path		=> '/etc/postfix/main.cf',
   source	=> '/root/puppet/files/postfix/main.cf',
-  notify	=> Service['pfx'],
+  notify	=> Service['psf'],
+}
+
+package { 'postfix':
+  ensure	=> present,
+  allow_virtual => true,
+  subscribe	=> File['main.cf'],
 }
 
 service { 'postfix':
-  alias		=> 'pfx',
+  alias		=> 'psf',
   ensure	=> running,
   enable	=> true,
 }
