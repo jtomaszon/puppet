8b5b30bca5048d73c5c4acfbc992df0aad3f123f
Javier Tomas Zon <jtomaszon@gmail.com>
2014-11-04 00:21:32 +0000

    Adding a module for postfix

---
+++ postfix/manifests/init.pp
@@ -0,0 +1,21 @@
+class postfix {
+  package {'postfix': ensure => present, allow_virtual => true }
+  file { 'main.cf':
+    ensure => present,
+    path => '/etc/postfix/main.cf',
+    owner => 'root',
+    group => 'root',
+    mode => 0644,
+    content => template('postfix/main.cf.erb'),
+    notify => Service['postfix'],
+    require => Package['postfix'],
+  }
+  service {'postfix':
+    ensure => running,
+    enable => true,
+    require => [ Package['postfix'], File['main.cf'],]
+  }
+
+}
+
+class {'postfix': }
